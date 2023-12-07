f3af23b5b7e08a5f109bbdbdd3ee3b9c94bf2208
nithin1456 <nithinaddanki244@gmail.com>
2023-12-07 21:44:59 +0530

    updated

--- decorator.py
+++ decorator.py
@@ -1,1 +1,27 @@
-print("hello")
+import time
+current_time = time.time()
+print(current_time)
+
+
+def speed_cal(function):                  
+    def wrapper():
+        start = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run time is {end_time-start}")
+    return wrapper
+
+
+@speed_cal
+def fast_fun():                                    
+    for i in range(100000000):
+        i*i
+
+@speed_cal
+def slow_fun():
+    for i in range(10000000000000):
+        i*i
+    
+
+fast_fun()
+slow_fun()
